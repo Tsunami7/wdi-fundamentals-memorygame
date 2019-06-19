61728881c7e031ba539604f5f8b616171f32f19a
Alex <abarba02@gmail.com>
2019-06-19 19:54:42 -0400

    created game logic

--- wdi-fundamentals-memorygame/memory_game/js/main.js
+++ wdi-fundamentals-memorygame/memory_game/js/main.js
@@ -1,32 +1,30 @@
 
 const cards = ["queen","queen","king","king"];
 const cardsInPlay = [];
-var cardOne=cards[0];
-var cardTwo=cards[1];
-var cardThree=cards[2];
-var cardFour=cards[3];
-/*cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-cardsInPlay.push(cardThree);
-cardsInPlay.push(cardFour);
-*/
 
-//console test
-console.log("User flipped queen "+cardsInPlay[0]);
-cardsInPlay.push(cardTwo);
-console.log("User flipped king "+cardsInPlay[1]);
+function checkForMatch(){
+  if (cardsInPlay[0]===cardsInPlay[1]) {
+    alert("you found a match!");
+  }else{
+    alert("Sorry try again");
+  }
+};
 
+function flipCard(cardId){
 
-const flipCard(cardId){
+  console.log("User flipped " + cards[cardId]);
+
+  cardsInPlay.push(cards[cardId]);
+
+  checkForMatch();
+
   //logic test if statement
   if(cardsInPlay.length===2){
   //console test
     console.log(cardsInPlay.length);
-    if (cardsInPlay[0]===cardsInPlay[1]) {
-      alert("you found a match!");
-    }else{
-      alert("Sorry try again");
-    }
+
   }
 
 };
+flipCard(0);
+flipCard(2);
